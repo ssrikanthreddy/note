4a0fb3f66bb89367a8a6e0763cae84a5766fb401
Srikanth Reddy <88000780+ssrikanthreddy@users.noreply.github.com>
2024-12-23 00:20:26 +0530

    Checkpoint: Added markdown shortcuts into slate.js

--- src/MainEditor.jsx
+++ src/MainEditor.jsx
@@ -1,69 +1,211 @@
-// Import React dependencies.
-import React, { useState, useCallback } from "react";
-// Import the Slate editor factory.
-import { createEditor } from "slate";
-import { Editor, Transforms, Element } from "slate";
-// Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from "slate-react";
+import React, { useCallback, useMemo } from "react";
+import {
+  createEditor,
+  Editor,
+  Element as SlateElement,
+  Node as SlateNode,
+  Point,
+  Range,
+  Transforms,
+} from "slate";
+import { withHistory } from "slate-history";
+import { Editable, ReactEditor, Slate, withReact } from "slate-react";
 
-const initialValue = [
-  {
-    type: "paragraph",
-    children: [{ text: "A line of text in a paragraph." }],
-  },
-];
-
-// Define a React component renderer for our code blocks.
-const CodeElement = (props) => {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  );
+const SHORTCUTS = {
+  "*": "list-item",
+  "-": "list-item",
+  "+": "list-item",
+  ">": "block-quote",
+  "#": "heading-one",
+  "##": "heading-two",
+  "###": "heading-three",
+  "####": "heading-four",
+  "#####": "heading-five",
+  "######": "heading-six",
 };
-
-const DefaultElement = (props) => {
-  return <p {...props.attributes}>{props.children}</p>;
-};
-
-// Define our app...
 const MainEditor = () => {
-  const [editor] = useState(() => withReact(createEditor()));
-
-  // Define a rendering function based on the element passed to `props`. We use
-  // `useCallback` here to memoize the function for subsequent renders.
-  const renderElement = useCallback((props) => {
-    switch (props.element.type) {
-      case "code":
-        return <CodeElement {...props} />;
-      default:
-        return <DefaultElement {...props} />;
-    }
-  }, []);
-
-  // Render the Slate context.
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const editor = useMemo(
+    () => withShortcuts(withReact(withHistory(createEditor()))),
+    []
+  );
+  const handleDOMBeforeInput = useCallback(
+    (e) => {
+      queueMicrotask(() => {
+        const pendingDiffs = ReactEditor.androidPendingDiffs(editor);
+        const scheduleFlush = pendingDiffs?.some(({ diff, path }) => {
+          if (!diff.text.endsWith(" ")) {
+            return false;
+          }
+          const { text } = SlateNode.leaf(editor, path);
+          const beforeText = text.slice(0, diff.start) + diff.text.slice(0, -1);
+          if (!(beforeText in SHORTCUTS)) {
+            return;
+          }
+          const blockEntry = Editor.above(editor, {
+            at: path,
+            match: (n) =>
+              SlateElement.isElement(n) && Editor.isBlock(editor, n),
+          });
+          if (!blockEntry) {
+            return false;
+          }
+          const [, blockPath] = blockEntry;
+          return Editor.isStart(editor, Editor.start(editor, path), blockPath);
+        });
+        if (scheduleFlush) {
+          ReactEditor.androidScheduleFlush(editor);
+        }
+      });
+    },
+    [editor]
+  );
   return (
-    // Add the editable component inside the context.
     <Slate editor={editor} initialValue={initialValue}>
       <Editable
+        onDOMBeforeInput={handleDOMBeforeInput}
         renderElement={renderElement}
-        onKeyDown={(event) => {
-          if (event.key === "`" && event.ctrlKey) {
-            // Prevent the "`" from being inserted by default.
-            event.preventDefault();
-            // Otherwise, set the currently selected blocks type to "code".
-            Transforms.setNodes(
-              editor,
-              { type: "code" },
-              {
-                match: (n) => Element.isElement(n) && Editor.isBlock(editor, n),
-              }
-            );
-          }
-        }}
+        placeholder="Write some markdown..."
+        spellCheck
+        autoFocus
       />
     </Slate>
   );
 };
-
+const withShortcuts = (editor) => {
+  const { deleteBackward, insertText } = editor;
+  editor.insertText = (text) => {
+    const { selection } = editor;
+    if (text.endsWith(" ") && selection && Range.isCollapsed(selection)) {
+      const { anchor } = selection;
+      const block = Editor.above(editor, {
+        match: (n) => SlateElement.isElement(n) && Editor.isBlock(editor, n),
+      });
+      const path = block ? block[1] : [];
+      const start = Editor.start(editor, path);
+      const range = { anchor, focus: start };
+      const beforeText = Editor.string(editor, range) + text.slice(0, -1);
+      const type = SHORTCUTS[beforeText];
+      if (type) {
+        Transforms.select(editor, range);
+        if (!Range.isCollapsed(range)) {
+          Transforms.delete(editor);
+        }
+        const newProperties = {
+          type,
+        };
+        Transforms.setNodes(editor, newProperties, {
+          match: (n) => SlateElement.isElement(n) && Editor.isBlock(editor, n),
+        });
+        if (type === "list-item") {
+          const list = {
+            type: "bulleted-list",
+            children: [],
+          };
+          Transforms.wrapNodes(editor, list, {
+            match: (n) =>
+              !Editor.isEditor(n) &&
+              SlateElement.isElement(n) &&
+              n.type === "list-item",
+          });
+        }
+        return;
+      }
+    }
+    insertText(text);
+  };
+  editor.deleteBackward = (...args) => {
+    const { selection } = editor;
+    if (selection && Range.isCollapsed(selection)) {
+      const match = Editor.above(editor, {
+        match: (n) => SlateElement.isElement(n) && Editor.isBlock(editor, n),
+      });
+      if (match) {
+        const [block, path] = match;
+        const start = Editor.start(editor, path);
+        if (
+          !Editor.isEditor(block) &&
+          SlateElement.isElement(block) &&
+          block.type !== "paragraph" &&
+          Point.equals(selection.anchor, start)
+        ) {
+          const newProperties = {
+            type: "paragraph",
+          };
+          Transforms.setNodes(editor, newProperties);
+          if (block.type === "list-item") {
+            Transforms.unwrapNodes(editor, {
+              match: (n) =>
+                !Editor.isEditor(n) &&
+                SlateElement.isElement(n) &&
+                n.type === "bulleted-list",
+              split: true,
+            });
+          }
+          return;
+        }
+      }
+      deleteBackward(...args);
+    }
+  };
+  return editor;
+};
+const Element = ({ attributes, children, element }) => {
+  switch (element.type) {
+    case "block-quote":
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case "bulleted-list":
+      return <ul {...attributes}>{children}</ul>;
+    case "heading-one":
+      return <h1 {...attributes}>{children}</h1>;
+    case "heading-two":
+      return <h2 {...attributes}>{children}</h2>;
+    case "heading-three":
+      return <h3 {...attributes}>{children}</h3>;
+    case "heading-four":
+      return <h4 {...attributes}>{children}</h4>;
+    case "heading-five":
+      return <h5 {...attributes}>{children}</h5>;
+    case "heading-six":
+      return <h6 {...attributes}>{children}</h6>;
+    case "list-item":
+      return <li {...attributes}>{children}</li>;
+    default:
+      return <p {...attributes}>{children}</p>;
+  }
+};
+const initialValue = [
+  {
+    type: "paragraph",
+    children: [
+      {
+        text: 'The editor gives you full control over the logic you can add. For example, it\'s fairly common to want to add markdown-like shortcuts to editors. So that, when you start a line with "> " you get a blockquote that looks like this:',
+      },
+    ],
+  },
+  {
+    type: "block-quote",
+    children: [{ text: "A wise quote." }],
+  },
+  {
+    type: "paragraph",
+    children: [
+      {
+        text: 'Order when you start a line with "## " you get a level-two heading, like this:',
+      },
+    ],
+  },
+  {
+    type: "heading-two",
+    children: [{ text: "Try it out!" }],
+  },
+  {
+    type: "paragraph",
+    children: [
+      {
+        text: 'Try it out for yourself! Try starting a new line with ">", "-", or "#"s.',
+      },
+    ],
+  },
+];
 export default MainEditor;
