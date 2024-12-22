2e29519e3d2543f3e085d457f8b95a0420a14129
Srikanth Reddy <88000780+ssrikanthreddy@users.noreply.github.com>
2024-12-23 00:09:50 +0530

    Checkpoint: Plugged in slate editor

---
+++ src/MainEditor.jsx
@@ -0,0 +1,69 @@
+// Import React dependencies.
+import React, { useState, useCallback } from "react";
+// Import the Slate editor factory.
+import { createEditor } from "slate";
+import { Editor, Transforms, Element } from "slate";
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from "slate-react";
+
+const initialValue = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+];
+
+// Define a React component renderer for our code blocks.
+const CodeElement = (props) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = (props) => {
+  return <p {...props.attributes}>{props.children}</p>;
+};
+
+// Define our app...
+const MainEditor = () => {
+  const [editor] = useState(() => withReact(createEditor()));
+
+  // Define a rendering function based on the element passed to `props`. We use
+  // `useCallback` here to memoize the function for subsequent renders.
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
+  // Render the Slate context.
+  return (
+    // Add the editable component inside the context.
+    <Slate editor={editor} initialValue={initialValue}>
+      <Editable
+        renderElement={renderElement}
+        onKeyDown={(event) => {
+          if (event.key === "`" && event.ctrlKey) {
+            // Prevent the "`" from being inserted by default.
+            event.preventDefault();
+            // Otherwise, set the currently selected blocks type to "code".
+            Transforms.setNodes(
+              editor,
+              { type: "code" },
+              {
+                match: (n) => Element.isElement(n) && Editor.isBlock(editor, n),
+              }
+            );
+          }
+        }}
+      />
+    </Slate>
+  );
+};
+
+export default MainEditor;
